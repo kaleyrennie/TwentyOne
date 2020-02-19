f0c601a9ebf00cb46f495658c26231c8bdd097a9
Kaley <kaleyrennie14@gmail.com>
2020-02-19 00:51:54 -0800

    Add project files.

---
+++ TwentyOne/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Card
+    {
+        public Card()
+        {
+            Suit = "Spades";
+            Face = "Two";
+        }
+        public string Suit { get; set; }
+        public string Face { get; set; }
+    }
+}
